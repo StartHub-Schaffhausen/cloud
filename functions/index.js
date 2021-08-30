f3bfd6a1ec969fd1a5cc64de3da5b55f31e802af
Sandro Scalco <sandro.scalco@liitu.ch>
2021-08-30 22:46:24 +0200

    new date

--- functions/index.js
+++ functions/index.js
@@ -685,10 +685,10 @@ exports.updateInvoiceStripeWebHook = functions.region('europe-west6').https.onRe
                         name: 'MeetinPointReservation',
                         data: {
                             tisch: reservation.data().desk.name,
-                            startDatum: reservation.data().dateFrom.toISOString().substring(8, 10) + "." + reservation.data().dateFrom.toISOString().substring(5, 7) + "." + reservation.data().dateFrom.toISOString().substring(0, 4),
-                            startUhrzeit: reservation.data().dateFrom.toISOString().substring(11, 16),
-                            endeDatum: reservation.data().dateTo.toISOString().substring(8, 10) + "." + reservation.data().dateTo.toISOString().substring(5, 7) + "." + reservation.data().dateTo.toISOString().substring(0, 4),
-                            endeUhrzeit: reservation.data().dateTo.toISOString().substring(11, 16),
+                            startDatum: new Date(reservation.data().dateFrom._seconds *1000).toISOString().substring(8, 10) + "." + new Date(reservation.data().dateFrom._seconds *1000).toISOString().substring(5, 7) + "." + new Date(reservation.data().dateFrom._seconds *1000).toISOString().substring(0, 4),
+                            startUhrzeit: new Date(reservation.data().dateFrom._seconds *1000).toISOString().substring(11, 16),
+                            endeDatum: new Date(reservation.data().dateTo._seconds *1000).toISOString().substring(8, 10) + "." + new Date(reservation.data().dateTo._seconds *1000).toISOString().substring(5, 7) + "." + new Date(reservation.data().dateTo._seconds *1000).toISOString().substring(0, 4),
+                            endeUhrzeit: new Date(reservation.data().dateTo._seconds *1000).toISOString().substring(11, 16),
                             stripeInvoiceUrl: invoiceData.stripeInvoiceUrl
                         },
                     },
